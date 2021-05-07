60acfae65c98908a32e8e2493f5314976a28bd32
Michael Bravo <mbravo@synopsys.com>
2021-05-06 19:59:59 -0700

    hardcoded credential defect

---
+++ src/main/java/HardCodedCred.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class HardCodedCred {
+    public static void main(String [] args) {
+        String DB_URL = "http://localhost:80";
+        String USER = "guest";
+        String PASS = "guestpw";
+        
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, 
+            // USER, PASS)
+            /*\
+            /*username*/ "guest",
+            /*password*/ "guestpw");
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        } 
+
+    }
+
+}
+
